1ed5eb65e6608005281cb43fc6e70cbbc9c95d69
James Koonts <jkoontsiii@gmail.com>
2023-02-16 00:04:45 -0500

    .

--- src/manage_clipboard/mod.rs
+++ src/manage_clipboard/mod.rs
@@ -14,5 +14,63 @@ trait Clipping {
 }
 
 impl Clipping for Clipboard {
+    fn set_clip_board_one() {
+        todo!()
+    }
+
+    fn set_clip_board_two() {
+        todo!()
+    }
+
+    fn set_clip_board_three() {
+        todo!()
+    }
+
+    fn set_clip_board_four() {
+        todo!()
+    }
+
+    fn set_clip_board_five() {
+        todo!()
+    }
+}
+
+
+fn set_clip_board(selection: i32, content: String) {
+
+    let mc = magic_crypt::new_magic_crypt!("scrumdiddlyumptious", 256);
+
+
+    // let encrypted = mc.encrypt_str_to_base64(data);
+    // println!("{}", encrypted);
+    //
+    // fs::write("/home/foxx/.sekret_enc", encrypted.as_bytes()).unwrap();
+
+    let file_read = fs::read("/home/foxx/.sekret_enc").unwrap();
+    let df: &str = std::str::from_utf8(file_read.as_slice()).unwrap();
+
+    let decrypted = mc.decrypt_base64_to_string(df).unwrap();
+    println!("{}", decrypted);
+
+}
+
+fn read_clip_board (selection: i32) {
+
+    let path: &str = "/home/foxx/.sekret";
+    let path2: &str = "/home/foxx/.sekret_enc";
+
+    let data = match fs::read_to_string(path) {
+        Ok(x) => x,
+        Err(_) => "ERR".to_string(),
+    };
+    // let encoded: String = general_purpose::STANDARD_NO_PAD.encode(data);
+    let mc = magic_crypt::new_magic_crypt!("scrumdiddlyumptious", 256);
+
+
+    // let encrypted = mc.encrypt_str_to_base64(data);
+    // println!("{}", encrypted);
+    //
+    // fs::write("/home/foxx/.sekret_enc", encrypted.as_bytes()).unwrap();
+
 
 }
