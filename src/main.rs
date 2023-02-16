b22cfc734283d40eb7f5bc282cc9a0ffceee5d6a
JamesK <james.k@konnektive.com>
2023-02-16 11:39:44 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -12,30 +12,27 @@ mod manage_clipboard;
 
 
 fn main() {
-    let path: &str = "/home/foxx/.sekret";
-    // let path2: &str = "/home/foxx/.sekret_enc";
-
-    let data = match fs::read_to_string(path) {
-        Ok(x) => x,
-        Err(_) => "ERR".to_string(),
-    };
-
+    // encryption algo
     let mc = magic_crypt::new_magic_crypt!("scrumdiddlyumptious", 256);
 
+    // // read plain and encrypt
+    // let path: &str = "/home/foxx/.sekret";
+    // let data = match fs::read_to_string(path) {
+    //     Ok(x) => x,
+    //     Err(_) => "ERR".to_string(),
+    // };
+    // let encrypted = mc.encrypt_str_to_base64(data);
+    // // print encrypted and write to file
+    // println!("{}", encrypted);
+    // fs::write("/home/foxx/.sekret_enc", encrypted.as_bytes()).unwrap();
 
-    let encrypted = mc.encrypt_str_to_base64(data);
-    println!("{}", encrypted);
 
-    fs::write("/home/foxx/.sekret_enc", encrypted.as_bytes()).unwrap();
-
+    // read encrypted
     let file_read = fs::read("/home/foxx/.sekret_enc").unwrap();
-
 
     let df: &str = std::str::from_utf8(file_read.as_slice()).unwrap();
     let decrypted = mc.decrypt_base64_to_string(df).unwrap();
     println!("{}", decrypted);
-
-
 
     // let mut enigo = Enigo::new();
     // thread::sleep(Duration::new(1,0));
