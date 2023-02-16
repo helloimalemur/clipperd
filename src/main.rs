9391d62170966e61b0c7a002f8b1c974a1898ec3
Koonts <37273704+helloimalemur@users.noreply.github.com>
2023-02-15 23:06:27 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 // https://lib.rs/crates/arboard
-// https://github.com/obv-mikhail/InputBot
+// https://crates.io/crates/magic-crypt
 // https://docs.rs/enigo/latest/enigo/
 // https://github.com/enigo-rs/enigo
-use arboard::Clipboard;
 use enigo::*;
+use std::{fs, thread};
+use std::time::Duration;
+use magic_crypt::MagicCryptTrait;
+
 
 fn main() {
-    let mut clipboard = Clipboard::new().unwrap();
-    println!("{}", clipboard.get().text().unwrap());
+    let path: &str = "/home/foxx/.sekret";
+
+    let data = match fs::read_to_string(path) {
+        Ok(x) => x,
+        Err(_) => "ERR".to_string(),
+    };
+
+    // let mc = magic_crypt::new_magic_crypt!("scrumdiddlyumptious", 256);
+
+    // let decrypted = mc.decrypt_base64_to_string(data).unwrap();
+    // println!("{}", decrypted);
 
     let mut enigo = Enigo::new();
-    enigo.key_sequence("asfd");
-
-
+    thread::sleep(Duration::new(1,0));
+    enigo.key_sequence(data.as_str());
 }
