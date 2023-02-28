21cd8b128cf94a5ec2d64c8df343295f28ce2454
foxx <jkoontsiii@gmail.com>
2023-02-27 22:03:45 -0500

    .

--- src/manage_clipboard.rs
+++ src/manage_clipboard.rs
@@ -137,7 +137,32 @@ impl Clipping for Clippard {
 
 fn set_clip_board(selection: i32, content: String) {
 // do some encryption and keep it in memory, dont write this to disk
+    // Example encryption and Enigo output to keyboard
+    // encryption algo
+    // let mc = magic_crypt::new_magic_crypt!("scrumdiddlyumptious", 256);
 
+    // // read plain and encrypt
+    // let path: &str = "/home/foxx/.sekret";
+    // let data = match fs::read_to_string(path) {
+    //     Ok(x) => x,
+    //     Err(_) => "ERR".to_string(),
+    // };
+    // let encrypted = mc.encrypt_str_to_base64(data);
+    // // print encrypted and write to file
+    // println!("{}", encrypted);
+    // fs::write("/home/foxx/.sekret_enc", encrypted.as_bytes()).unwrap();
+
+
+    // read encrypted
+    // let file_read = fs::read("/home/foxx/.sekret_enc").unwrap();
+    //
+    // let df: &str = std::str::from_utf8(file_read.as_slice()).unwrap();
+    // let decrypted = mc.decrypt_base64_to_string(df).unwrap();
+    // println!("{}", decrypted);
+
+    // let mut enigo = Enigo::new();
+    // thread::sleep(Duration::new(1,0));
+    // enigo.key_sequence(data.as_str());
 
 
 }
