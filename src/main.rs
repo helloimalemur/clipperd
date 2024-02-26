1ed408c97e0d482df5c977a4a88b932811ae77dc
foxx <jkoontsiii@gmail.com>
2024-02-26 00:50:23 -0500

    ref

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use crate::manage_clipboard::start_daemon;
 mod manage_clipboard;
 extern crate daemonize;
@@ -5,14 +6,15 @@ use std::fs::File;
 use daemonize::Daemonize;
 
 fn main() {
+    let _ = fs::create_dir_all("~/.clipperd").unwrap();
     // TODO: debug daemon not writing out via onigo
-    let stdout = File::create("/var/lib/clippard/daemon.out").unwrap();
-    let stderr = File::create("/var/lib/clippard/daemon.err").unwrap();
+    let stdout = File::create("~/.clipperddaemon.out").unwrap();
+    let stderr = File::create("~/.clipperddaemon.err").unwrap();
 
     let daemonize = Daemonize::new()
-        .pid_file("/var/lib/clippard/clippard.pid") // Every method except `new` and `start`
+        .pid_file("~/.clipperdclippard.pid") // Every method except `new` and `start`
         .chown_pid_file(true)      // is optional, see `Daemonize` documentation
-        .working_directory("/var/lib/clippard/") // for default behaviour.
+        .working_directory("~/.clipperd") // for default behaviour.
         .user("root")
         .group("root") // Group name
         .group(2)        // or group id.
